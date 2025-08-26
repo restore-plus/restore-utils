6f0cd1224160350557969d0cf11ccde155a69210
Felipe Carlos <efelipecarlos@gmail.com>
2025-08-26 06:17:46 -0300

    prodes: add loader option

--- R/prodes.R
+++ R/prodes.R
@@ -10,7 +10,7 @@
 }
 
 #' @export
-prodes_generate_forest_mask <- function(target_year, version = "v2", multicores = 32, memsize = 120) {
+prodes_generate_forest_mask <- function(target_year, version = "v2", multicores = 32, memsize = 120, prodes_loader = load_prodes_2024) {
     if (target_year >= 2024) {
         cli::cli_abort(
             "Nothing to do: 2024 is the most recent year; the forest in this year represents the actual available forest"
@@ -33,7 +33,7 @@ prodes_generate_forest_mask <- function(target_year, version = "v2", multicores 
     # Load PRODES 2024 cube
     # Why 2024 ? This is the current version of PRODES available. We always go
     # from the latest PRODES year available.
-    prodes_cube <- load_prodes_2024(
+    prodes_cube <- prodes_loader(
         version    = version,
         multicores = multicores,
         memsize    = memsize
@@ -88,7 +88,56 @@ load_prodes_2000 <- function(version = "v2", multicores = 32, memsize = 120) {
                            "tile", "start_date", "end_date",
                            "band", "version"),
             bands = "class",
+            labels = c("0"  = "d2000",
+                       "50" = "r2010",
+                       "51" = "r2011",
+                       "52" = "r2012",
+                       "53" = "r2013",
+                       "54" = "r2014",
+                       "55" = "r2015",
+                       "56" = "r2016",
+                       "57" = "r2017",
+                       "58" = "r2018",
+                       "59" = "r2019",
+                       "60" = "r2020",
+                       "61" = "r2021",
+                       "62" = "r2022",
+                       "63" = "r2023",
+                       "64" = "r2024",
+                       "91"  = "Hidrografia",
+                       "100" = "Vegetação Nativa",
+                       "101" = "Não Floresta")
+        )
+
+        saveRDS(prodes, prodes_rds)
+    }
+    prodes
+}
+
+#' @export
+load_prodes_2005 <- function(version = "v2", multicores = 32, memsize = 120) {
+    prodes_dir <- .prodes_dir(version = version, year = 2005)
+    prodes_rds <- .prodes_rds(prodes_dir)
+
+    if (fs::file_exists(prodes_rds)) {
+
+        prodes <- readRDS(prodes_rds)
+
+    } else {
+        # Recover the PRODES classified cube
+        prodes <- sits_cube(
+            source = "MPC",
+            collection = "LANDSAT-C2-L2",
+            data_dir = prodes_dir,
+            multicores = multicores,
+            memsize = memsize,
+            parse_info = c("product", "sensor",
+                           "tile", "start_date", "end_date",
+                           "band", "version"),
+            bands = "class",
             labels = c("0"   = "d2000",
+                       "2"   = "d2002",
+                       "4"   = "d2004",
                        "91"  = "Hidrografia",
                        "100" = "Vegetação Nativa",
                        "101" = "Não Floresta")
