67f1be7e8049df032100a7ce8ab6e1969c4b1a38
Felipe Carlos <efelipecarlos@gmail.com>
2026-02-24 19:19:45 -0300

    labels: add cerrado labels for a single classification
    Co-authored-by: Felipe Carvalho <lipecaso@gmail.com>

--- R/restore_labels.R
+++ R/restore_labels.R
@@ -1,12 +1,12 @@
 #' @title Labels Amazon MCTI
-#' 
+#'
 #' @author Felipe Carlos, \email{efelipecarlos@@gmail.com}
 #' @author Felipe Carvalho, \email{felipe.carvalho@@inpe.br}
-#' 
+#'
 #' @description Returns the labels for the Amazon maps produced for the MCTI.
-#' 
+#'
 #' @returns A vector of labels.
-#' 
+#'
 #' @export
 labels_amazon_mcti <- function() {
     c(
@@ -24,5 +24,31 @@ labels_amazon_mcti <- function() {
         "12" = "Desmatamento do ano",
         "13" = "Agricultura Perene",
         "14" = "Não observado"
+    )
+}
+
+#' @title Labels Cerrado of a single classification
+#'
+#' @author Felipe Carlos, \email{efelipecarlos@@gmail.com}
+#' @author Felipe Carvalho, \email{felipe.carvalho@@inpe.br}
+#'
+#' @description Returns the labels for the Cerrado classification.
+#'
+#' @returns A vector of labels.
+#'
+#' @export
+labels_cerrado_classification <- function() {
+    c(
+        "0"  = "NoData",
+        "1"  = "Agricultura anual",
+        "2"  = "Cerradao",
+        "3"  = "Cerrado",
+        "4"  = "Natural Não-Vegetal",
+        "5"  = "Open-Cerrado",
+        "6"  = "Pasto",
+        "7"  = "Agricultura Perene",
+        "8"  = "Silvicultura",
+        "9"  = "Cana",
+        "10" = "Água"
     )
 }
