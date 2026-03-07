3c6c65337d48e3a9887fe142c8470496fcc88a83
Felipe Carlos <efelipecarlos@gmail.com>
2026-03-07 02:27:34 -0300

    terraclass: add non-observed fix for terraclass 2024
    Co-authored-by: Felipe Carvalho <lipecaso@gmail.com>

--- R/terraclass_download.R
+++ R/terraclass_download.R
@@ -252,7 +252,7 @@ prepare_terraclass <- function(years, region_id, fix_non_observed = TRUE,
         cli::cli_inform("Mask update: Fixing non-observed")
 
         # Define valid years
-        valid_years <- c(2022, 2020, 2018, 2014, 2012, 2010, 2008)
+        valid_years <- c(2024, 2022, 2020, 2018, 2014, 2012, 2010, 2008)
 
         stopifnot(all(valid_years %in% years))
 
